87da63a94a869684b3f0cafa2a1a1ab25fa9078e
zaen323 <chouzaen2002@gmail.com>
2017-11-30 12:36:16 +0900

    truncate more

--- journal_dump.sh
+++ journal_dump.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-journalctl -n 14 | sed 1d | awk '{$1=$2=$4=""; print $0}' | sed 's/  //' | sed 's/  / /' | cut -c1-90 > ~/.config/conky/.tmp/journal.txt
+journalctl -n 14 | sed 1d | awk '{$1=$2=$4=""; print $0}' | sed 's/  //' | sed 's/  / /' | cut -c1-84 > ~/.config/conky/.tmp/journal.txt
