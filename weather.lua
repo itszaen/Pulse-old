67f36d718a5bb0844bf0db03ab47967242659bcf
zaen323 <chouzaen2002@gmail.com>
2018-01-03 22:48:22 +0900

    add forecast (with bug: the second one lacks icon)

--- weather.lua
+++ weather.lua
@@ -293,6 +293,178 @@ function weather(x,y)
   iconsize = 81
   iconorig = 30
   draw_image(iconx,icony,iconname,iconsize,iconorig,color6)
+
+  x = x15 + 15
+  y = y15 + 50
+  forecasts(x,y)
+
+end
+function forecasts(x,y)
+  text = "3 Day Forecasts"
+  font = "Roboto"
+  font_size = 16.5
+  text_extents(text,font,font_size)
+  color = color6
+
+  top = 5
+  left = 5
+  spacing = 70
+  spacing_text = 18
+  weather_icon_size = 65
+  weather_icon_offset = 10
+  text_indent = 70  -- from left
+  text_spacing = 25 -- from top
+  icon_offset = -15
+  icon_size = 20
+  text_icon_indent  = 50
+  text_icon_indent2 = 25
+  text_text_indent  = 18
+  text_text_indent2 = 20
+
+  -- title
+  sx1 = x
+  sy1 = y + (extents.height/2 + extents.y_bearing)
+  fx1 = sx1 + 40
+  fy1 = sy1
+  cairo_set_source_rgba(cr,rgba(color))
+  cairo_set_line_width(cr,2)
+  draw_line(sx1,sy1,fx1,fy1)
+  cairo_stroke(cr)
+
+  x1 = fx1 + 5
+  y1 = y
+  displaytext(x1,y,text,font,font_size,color)
+
+  sx2 = x1 + (extents.width + extents.x_bearing) + 5
+  sy2 = sy1
+  fx2 = sx2 + 40
+  fy2 = sy2
+  cairo_set_source_rgba(cr,rgba(color))
+  cairo_set_line_width(cr,2)
+  draw_line(sx2,sy2,fx2,fy2)
+  cairo_stroke(cr)
+
+  -- tommorow
+  font_size = 13
+  secondx = x + left
+  secondy = y + top
+  iconx1 = secondx
+  icony1 = secondy + weather_icon_offset
+  draw_image(iconx1,icony1,"weather2",weather_icon_size,30,color)
+  x2 = secondx + text_indent
+  y2 = secondy + text_spacing
+  text = "Tomorrow " .. second_date .. ":"
+  displaytext(x2,y2,text,font,font_size,color)
+
+  x3 = x2
+  y3 = y2 + spacing_text
+  text = second_summary
+  displaytext(x3,y3,text,font,font_size,color)
+
+  x4 = x3
+  y4 = y3 + spacing_text
+  text = second_temp_high .. "/" .. second_temp_low
+  displaytext(x4,y4,text,font,font_size,color)
+
+  iconx2 = x4 + text_icon_indent
+  icony2 = y4 + icon_offset
+  iconname = "humidity"
+  draw_image(iconx2,icony2,iconname,icon_size,30,color)
+  x5 = iconx2 + text_text_indent
+  y5 = y4
+  text = second_humidity
+  displaytext(x5,y5,text,font,font_size,color)
+
+  iconx3 = x5 + text_icon_indent2
+  icony3 = y5 + icon_offset
+  iconname = "umbrella"
+  draw_image(iconx3,icony3,iconname,icon_size,30,color)
+  x6 = iconx3 + text_text_indent2
+  y6 = y5
+  text = second_precip_chance
+  displaytext(x6,y6,text,font,font_size,color)
+
+  font_size = 13
+
+  -- day after tomorrow
+  thirdx = secondx
+  thirdy = secondy + spacing
+  iconx1 = thirdx
+  icony1 = thirdx + weather_icon_offset
+  draw_image(iconx1,icony1,"weather3",weather_icon_size,30,color)
+
+  x2 = thirdx + text_indent
+  y2 = thirdy + text_spacing
+  text = third_day_of_week .. " " .. third_date .. ":"
+  displaytext(x2,y2,text,font,font_size,color)
+
+  x3 = x2
+  y3 = y2 + spacing_text
+  text = third_summary
+  displaytext(x3,y3,text,font,font_size,color)
+
+  x4 = x3
+  y4 = y3 + spacing_text
+  text = third_temp_high .. "/" .. third_temp_low
+  displaytext(x4,y4,text,font,font_size,color)
+
+  iconx2 = x4 + text_icon_indent
+  icony2 = y4 + icon_offset
+  iconname = "humidity"
+  draw_image(iconx2,icony2,iconname,icon_size,30,color)
+  x5 = iconx2 + text_text_indent2
+  y5 = y4
+  text = third_humidity
+  displaytext(x5,y5,text,font,font_size,color)
+
+  iconx3 = x5 + text_icon_indent2
+  icony3 = y5 + icon_offset
+  iconname = "umbrella"
+  draw_image(iconx3,icony3,iconname,icon_size,30,color)
+  x6 = iconx3 + text_text_indent2
+  y6 = y5
+  text = third_precip_chance
+  displaytext(x6,y6,text,font,font_size,color)
+
+  -- 2 days after tomorrow
+  font_size = 13
+  fourthx = thirdx
+  fourthy = thirdy + spacing
+  iconx1 = fourthx
+  icony1 = fourthy + weather_icon_offset
+  draw_image(iconx1,icony1,"weather4",weather_icon_size,30,color)
+  x2 = fourthx + text_indent
+  y2 = fourthy + text_spacing
+  text = fourth_day_of_week .. " " .. fourth_date .. ":"
+  displaytext(x2,y2,text,font,font_size,color)
+
+  x3 = x2
+  y3 = y2 + spacing_text
+  text = fourth_summary
+  displaytext(x3,y3,text,font,font_size,color)
+
+  x4 = x3
+  y4 = y3 + spacing_text
+  text = fourth_temp_high .. "/" .. fourth_temp_low
+  displaytext(x4,y4,text,font,font_size,color)
+
+  iconx2 = x4 + text_icon_indent
+  icony2 = y4 + icon_offset
+  iconname = "humidity"
+  draw_image(iconx2,icony2,iconname,icon_size,30,color)
+  x5 = iconx2 + text_text_indent
+  y5 = y4
+  text = fourth_humidity
+  displaytext(x5,y5,text,font,font_size,color)
+
+  iconx3 = x5 + text_icon_indent2
+  icony3 = y5 + icon_offset
+  iconname = "umbrella"
+  draw_image(iconx3,icony3,iconname,icon_size,30,color)
+  x6 = iconx3 + text_text_indent2
+  y6 = y5
+  text = fourth_precip_chance
+  displaytext(x6,y6,text,font,font_size,color)
 end
 function store_weather_icons()
   path = curdir .. "/image/weather_icons/celsius.svg"
@@ -336,19 +508,18 @@ function store_weather_condition_icons()
   path = curdir .. "/image/weather_icons/" .. weather_icon_name(summary) ..".svg"
   name = "weather"
   store_image(path,name)
-  print(second_summary)
-  print(third_summary)
-  print(fourth_summary)
-  --[[path = curdir .. "/image/weather_icons/" .. weather_icon_name(second_summary) .. ".svg"
+  path = curdir .. "/image/weather_icons/" .. forecast_icon_name(second_summary) .. ".svg"
   name = "weather2"
   store_image(path,name)
-  path = curdir .. "/image/weather_icons/" .. weather_icon_name(third_summary) .. ".svg"
+  path = curdir .. "/image/weather_icons/" .. forecast_icon_name(third_summary) .. ".svg"
   name = "weather3"
   store_image(path,name)
-  path = curdir .. "/image/weather_icons/" .. weather_icon_name(fourth_summary) .. ".svg"
+  path = curdir .. "/image/weather_icons/" .. forecast_icon_name(fourth_summary) .. ".svg"
   name = "weather4"
   store_image(path,name)
-  ]]
+end
+function forecast_icon_name(weather)
+  return weather_icon_t[weather]
 end
 function weather_icon_name(weather)
   if 5 < tonumber(hours) and tonumber(hours) < 18  then
