39a62d5c6263f65914c574e4f757d72146ce0fcb
zaen323 <chouzaen2002@gmail.com>
2018-01-10 17:00:40 +0900

    see diff.

--- timetable.lua
+++ timetable.lua
@@ -1,4 +1,42 @@
 function classinfo(advance,wintertime)
+  local hours = tonumber(hours)
+  local minutes = tonumber(minutes)
+  local seconds = tonumber(seconds)
+
+  curtime = hours*3600 + minutes*60 + seconds -- in seconds
+  advance = advance*60 -- in seconds
+  starttime = 6 * 3600 + 0 * 60 + 0
+
+  if conky_start == 1 then
+    file = io.open(curdir .. "/timetable.txt")
+    classname_t = {}
+    for line in file:lines() do
+      table.insert(classname_t,line)
+    end
+    file:close()
+  end
+  if conky_start == 1 then
+    if wintertime == 0 then
+      classtime_t = {
+        {{8,10},{9,10},{10,10},{11,10},{12,40},{13,40}},
+        {{8,10},{9,10},{10,10},{11,10},{12,40},{13,40}},
+        {{8,10},{9,10},{10,10},{11,5},{11,40},{13,10},{14,10}},
+        {{8,10},{9,10},{10,10},{11,10},{12,40},{13,40}},
+        {{8,10},{9,10},{10,10},{11,10},{12,40},{13,40}},
+        {{8,10},{9,10},{10,10},{11,10}}
+      }
+    else
+      classtime_t = {
+        {{8,20},{9,20},{10,20},{11,20},{12,50},{13,50}},
+        {{8,20},{9,20},{10,20},{11,20},{12,50},{13,50}},
+        {{8,20},{9,20},{10,20},{11,15},{11,50},{13,20},{14,20}},
+        {{8,20},{9,20},{10,20},{11,20},{12,50},{13,50}},
+        {{8,20},{9,20},{10,20},{11,20},{12,50},{13,50}},
+        {{8,20},{9,20},{10,20},{11,20}}
+      }
+    end
+  end
+
   if isVacation() == 1 then
     return "Enjoy your summer vacation!",nil
   elseif isVacation() == 2 then
@@ -7,27 +45,28 @@ function classinfo(advance,wintertime)
     return "Enjoy your spring rest!",nil
   elseif isSunday() == 1 then
     return "It's your day off!",nil
-  elseif isSchoolFinished() == 1 then
+  elseif isSchoolFinished(classtime_t,curtime,starttime) == 1 then
     return "School finished!",nil
   else
-    class,time = classname(advance,wintertime)
-    return class,time
+    class,countdown = classname(classname_t,classtime_t,curtime,advance)
+    return class,countdown
   end
 end
-function classname(advance,wintertime)
-  local hours = tonumber(hours)
-  local minutes = tonumber(minutes)
-  number,time = classnumber(hours,minutes,advance,wintertime)
-  if conky_start == 1 then
-    file = io.open(curdir .. "/timetable.txt")
-    class_table = {}
-    for line in file:lines() do
-      table.insert(class_table,line)
-    end
-    file:close()
+function classname(classname_t,classtime_t,curtime,advance)
+  class = classname_t[number]
+
+  classtime_in_seconds_t = {}
+  for i in ipairs(classtime_weekday_t) do
+    classtime_in_seconds = classtime_weekday_t[i][1]*3600 + classtime_weekday_t[i][2]* 60 + 0
+    table.insert(classtime_in_seconds_t,classtime_in_seconds)
   end
-  class = class_table[number]
-  return class,time
+
+  classtime_weekday_t = classtime_t[weekday_number]
+
+  number = classnumber(classtime_t,classtime_in_seconds_t,curtime,advance)
+  now_classtime_in_second = classtime_weekday_t[class_number][1]*3600+classtime_weekday_t[class_number][2]*60 + 0
+  countdown = countdown(now_classtime_in_second)
+  return class,countdown
 end
 
 function isSunday()
@@ -62,80 +101,47 @@ function isVacation()
     return 0
   end
 end
-function isSchoolFinished()
-  local hours = tonumber(hours)
-  local weekday = tonumber(os.date("%w"))
-  if weekday == 6 then
-    if hours > 12 or hours < 3 then
-      return 1
-    else
-      return 0
-    end
+function isSchoolFinished(timetable_t,curtime,starttime)
+  classtime_weekday_t = classtime_t[weekday_number]
+  finishtime = table.remove(classtime_weekday_t)
+  table.insert(classtime_weekday_t,finishtime) -- return the value back to the table
+  finishtime_in_seconds = finishtime[1]*3600 + finishtime[2]*60 + 0
+  if finishtime_in_seconds <= curtime and curtime >= starttime then
+    return 0
   else
-    if hours > 15 or hours < 3 then
-      return 1
-    else
-      return 0
-    end
+    return 1
   end
 end
 
-function classnumber(hour,minute,advance,wintertime)
-  curtime_n = hour*60 + minute
-  time_n = curtime_n + advance
-  local weekday_n = tonumber(os.date("%w"))
-  local seconds_n = tonumber(seconds)
+function classnumber(classtime_t,classtime_in_seconds_t,curtime,advance)
+  modtime = curtime + advance
+  class_number = within(classtime_in_seconds_t,modtime) -- on its weekday
 
-  if conky_start == 1 then
-    if wintertime == 0 then
-      classtime_t = {
-        {{8,10},{9,10},{10,10},{11,10},{12,40},{13,40}},
-        {{8,10},{9,10},{10,10},{11,10},{12,40},{13,40}},
-        {{8,10},{9,10},{10,10},{11,5},{11,40},{13,10},{14,10}},
-        {{8,10},{9,10},{10,10},{11,10},{12,40},{13,40}},
-        {{8,10},{9,10},{10,10},{11,10},{12,40},{13,40}},
-        {{8,10},{9,10},{10,10},{11,10}}
-      }
-    else
-      classtime_t = {
-        {{8,20},{9,20},{10,20},{11,20},{12,50},{13,50}},
-        {{8,20},{9,20},{10,20},{11,20},{12,50},{13,50}},
-        {{8,20},{9,20},{10,20},{11,15},{11,50},{13,20},{14,20}},
-        {{8,20},{9,20},{10,20},{11,20},{12,50},{13,50}},
-        {{8,20},{9,20},{10,20},{11,20},{12,50},{13,50}},
-        {{8,20},{9,20},{10,20},{11,20}}
-      }
-    end
-    classtime_weekday_t = classtime_t[weekday_n]
-    classtime_n_t = {}
-    for i in ipairs(classtime_weekday_t) do
-      class_time_n = classtime_weekday_t[i][1] * 60 + classtime_weekday_t[i][2]
-      table.insert(classtime_n_t,class_time_n)
-    end
-  end
-  class_number = within(classtime_n_t,time_n) -- on its weekday
-  now_class_time_n = classtime_weekday_t[class_number][1]*60+classtime_weekday_t[class_number][2]
-  if weekday >= 2 then
+  -- add all the class numbers before the day (not for Monday)
+  if weekday_number >= 2 then
     local n = 0
-    for i=1,weekday-1 do
+    for i=1,weekday_number-1 do
       n = n + length_table(classtime_t[i])
     end
     class_number = class_number + n
   end
-  time = now_class_time_n*60 - (curtime_n*60 + seconds_n) -- seconds
-  if time <= 0 then
-    time = "now"
-  else
-    time = second2Mmss(time,":")
-  end
-
-  return class_number,time
+  return class_number
 end
-function within(table1,number)
-  for i in ipairs(table1) do
-    if table1[i+1] == nil then
+function countdown(time)
+  countdown = time - curtime -- seconds
+  if countdown <= 0 then
+    countdown = "now"
+  else
+    countdown = second2Mmss(time,":")
+  end
+end
+function within(T,number)
+  for i in ipairs(T) do
+    if number <= T[1] then
+      return 1
+    elseif T[i+1] == nil then
       break
-    elseif table1[i] <= number and number <= table1[i+1] then
+    elseif T[i] <= number and number <= T[i+1] then
       return i
     end
   end
